e1f46dd5f7548d6e084d7e8573a4342eb2c8013a
James Borden <jmborden@gmail.com>
2022-05-16 15:47:14 -0400

    clean up of code

--- R/download.R
+++ R/download.R
@@ -1,46 +1,11 @@
 library(fs)
-library(curl)
-library(stringr)
 library(purrr)
-options(timeout=60000)
+library(rvest)
+options(timeout=1800)
 
-cache_dir = "cache"
-fs::dir_create(cache_dir)
+download_dir <- "download"
+fs::dir_create(download_dir)
 
-ftp_base <- "ftp://ftp.uniprot.org"
-ftp_dir <- "pub/databases/uniprot/current_release/knowledgebase/complete"
-ftp_path <- file.path(ftp_base,ftp_dir)
-
-
-#https://gist.github.com/adamhsparks/18f7702906f33dd66788e0078979ff9a
-list_files <- curl::new_handle()
-curl::handle_setopt(list_files, ftp_use_epsv = TRUE, dirlistonly = TRUE)
-
-con <- curl::curl(url = paste0(ftp_path,"/"), "r", handle = curl::new_handle())
-files <- readLines(con)
-close(con)
-
-ftp_files <- files |> stringr::str_split("\\s") |>
-map(function(x) {
-    x |> purrr::discard(~ .x == "")
-}) |>
-map(function(x) {
-    f <- c()
-    f$perms <- x[[1]]
-    f$bytes <- x[[5]]
-    f$month <- x[[6]]
-    f$day <- x[[7]]
-    f$time <- x[[8]]
-    f$filename <- x[[9]]
-    f
-}) |> map(~ .x$filename) |>
-# We are only processing the uniprot_sprot.xml.gz file
-purrr::keep(~.x == "uniprot_sprot.xml.gz") |>
-map(~ file.path(ftp_path, .x)) |> unlist() |>
-unlist()
-
-ftp_files |>
-walk(~ {
-    filename <- basename(.x)
-    download.file(destfile=(file.path(cache_dir, filename)), url = .x)
-})
+print("Downloading Files")
+url='ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.xml.gz'
+download.file(url=url,destfile=(file.path(download_dir,"uniprot_sprot.xml.gz")))
