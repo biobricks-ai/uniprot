9e0ccc70b6eccf2fef2d0da659c05524ceb66f63
ajay-nagyal <anagyal@insilica.co>
2025-09-19 16:20:22 -0400

    out to brick

--- R/csv_to_parquet.R
+++ R/csv_to_parquet.R
@@ -1,4 +1,4 @@
 library(arrow)
 library(vroom)
-arrow::write_parquet(vroom::vroom("data/uniprot_sprot.csv"),
-    file.path("data/uniprot_sprot.parquet"))
+arrow::write_parquet(vroom::vroom("brick/uniprot_sprot.csv"),
+    file.path("brick/uniprot_sprot.parquet"))
