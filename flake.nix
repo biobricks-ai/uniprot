0f00fbadd2cba507ea70f6bb6fe87d3894069d28
ajay-nagyal <anagyal@insilica.co>
2025-09-19 16:20:14 -0400

    init(nix-flake)

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Uniprot BioBrick";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          buildInputs = [
+            R
+            rPackages.vroom
+            rPackages.arrow
+            pkgs.libxslt
+          ];
+        };
+      });
+}
